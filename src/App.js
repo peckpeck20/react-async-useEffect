dbed5874d715a1c33b71fb4e8f477000ff12d0ef
Jose Zapata <jose.zapata@columbiaroad.com>
2020-03-04 11:19:21 +0200

    async fetch in useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [state, setstate] = useState(null);
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch(
+          "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
+        );
+        const data = await response.json();
+        setstate(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchQuote();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='https://amc-theatres-res.cloudinary.com/image/upload/c_lfill,f_auto,fl_lossy,g_auto,q_auto,w_1600/v1553002407/amc-cdn/general/287a3bb6-673e-40e8-84c4-df12a9fabf26/Star-Wars-logo-header.jpg-2000-1427.jpg' className="App-logo" alt="logo" />
+        <p className='text'>{state && state.starWarsQuote}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
