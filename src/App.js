3f5261401d23f30d6acc90ddcb495db3be6a0f41
Jose Zapata <jose.zapata@columbiaroad.com>
2020-03-04 11:49:02 +0200

    fetch start wars movie info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+const Card = ({ item }) => (
+  <div className="text">
+    <h1>{item.title}</h1>
+    <h3>{item.opening_crawl}</h3>
+    <p>by {item.director}</p>
+  </div>
+);
+
 const App = () => {
   const [state, setstate] = useState(null);
   useEffect(() => {
     const fetchQuote = async () => {
       try {
-        const response = await fetch(
-          "https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
-        );
+        const response = await fetch("https://swapi.co/api/films/");
         const data = await response.json();
-        setstate(data);
+        setstate(data.results);
       } catch (error) {
         console.log(error);
       }
@@ -20,8 +26,16 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://amc-theatres-res.cloudinary.com/image/upload/c_lfill,f_auto,fl_lossy,g_auto,q_auto,w_1600/v1553002407/amc-cdn/general/287a3bb6-673e-40e8-84c4-df12a9fabf26/Star-Wars-logo-header.jpg-2000-1427.jpg' className="App-logo" alt="logo" />
-        <p className='text'>{state && state.starWarsQuote}</p>
+        <img
+          src="https://amc-theatres-res.cloudinary.com/image/upload/c_lfill,f_auto,fl_lossy,g_auto,q_auto,w_1600/v1553002407/amc-cdn/general/287a3bb6-673e-40e8-84c4-df12a9fabf26/Star-Wars-logo-header.jpg-2000-1427.jpg"
+          className="App-logo"
+          alt="logo"
+        />
+        {state ? (
+          state.map((x, y) => <Card item={x} key={y} />)
+        ) : (
+          <p>Loading..</p>
+        )}
       </header>
     </div>
   );
